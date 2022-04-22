95de0d0a29df06a6797268cf7a78f569568a9aaf
jtoledo5630@outlook.com <jtoledo5630@outlook.com>
2022-04-22 11:03:56 -0700

    Versión 1.0
    Se suben los cambios por primera vez

---
+++ ApiGetHumano/Controllers/HumanosController.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ApiGetHumano.Data;
+
+namespace ApiGetHumano.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HumanosController : ControllerBase
+    {
+        private readonly ApiGetHumanoContext _context;
+
+        public HumanosController(ApiGetHumanoContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Humanos/GetHumanoById/5
+        [HttpGet("GetHumanoById")]
+        public async Task<IActionResult> GetHumanoById(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var humano = await _context.Humano
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (humano == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(humano);
+        }
+    }
+}
---
+++ ApiGetHumano/Data/ApiGetHumanoContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ApiGetHumano.Modelo;
+
+namespace ApiGetHumano.Data
+{
+    public class ApiGetHumanoContext : DbContext
+    {
+        public ApiGetHumanoContext (DbContextOptions<ApiGetHumanoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ApiGetHumano.Modelo.Humano> Humano { get; set; }
+    }
+}
