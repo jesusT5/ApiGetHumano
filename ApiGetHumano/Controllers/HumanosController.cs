efe28ff8008aa8396c64b85fd3d000bbc5bbffb1
jtoledo5630@outlook.es <jtoledo5630@gmail.com>
2023-08-03 20:07:23 -0700

    Update
    Test

--- ApiGetHumano/Controllers/HumanosController.cs
+++ ApiGetHumano/Controllers/HumanosController.cs
@@ -24,7 +24,8 @@ namespace ApiGetHumano.Controllers
             return Ok(new List<Humano> { new Humano { Id = 1, Altura = 187, Edad = 31, Nombre = "Jesus1", Peso = 100, Sexo = "Masculino" },
             new Humano { Id = 1, Altura = 187, Edad = 31, Nombre = "Jesus2", Peso = 100, Sexo = "Masculino" },
             new Humano { Id = 1, Altura = 187, Edad = 31, Nombre = "Jesus3", Peso = 100, Sexo = "Masculino" },
-            new Humano { Id = 1, Altura = 187, Edad = 31, Nombre = "Jesus4", Peso = 100, Sexo = "Masculino" }
+            new Humano { Id = 1, Altura = 187, Edad = 31, Nombre = "Jesus4", Peso = 100, Sexo = "Masculino" },
+            new Humano { Id = 1, Altura = 187, Edad = 31, Nombre = "Jesus5", Peso = 100, Sexo = "Masculino" }
             });
         }
 
